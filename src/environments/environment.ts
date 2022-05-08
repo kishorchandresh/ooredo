dc8ecb012a8a0a24c4682f9ac41f3fd7dcb91a9c
chandresh kishor <36341620+kishorchandresh@users.noreply.github.com>
2022-05-08 14:49:36 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,7 @@ export const environment = {
   LOGO                            : "/assets/images/logo.PNG",
   GRAPH_DATA_URL                  : "https://run.mocky.io/v3/f01e7bd1-ced2-4cc1-8689-8a48c4ba9796",
 
-  MAP_API_KEY                     : "AIzaSyAUNuAacMSVPScuMl1W5OuseyE2AzTMkos"
+//   MAP_API_KEY                     : "Plz Add Your Api key"
+  MAP_API_KEY                     : " AIzaSyAUNuAacMSVPScuMl1W5OuseyE2AzTMkos"
+ 
 };
